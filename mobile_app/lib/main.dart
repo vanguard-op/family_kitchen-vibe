abe82f15591ae4947a57d74c403d23ed5bdd63f6
Ahmad Khidir <khidirahmad05@gmail.com>
2026-03-27 14:34:44 +0100

    feat: Refactor main.dart by removing unnecessary code and update pubspec.yaml with new dependencies for enhanced functionality

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,33 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'screens/splash_screen.dart';
-import 'providers/auth_provider.dart';
-import 'config/routes.dart';
+import 'app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const FamilyKitchenApp());
-}
-
-class FamilyKitchenApp extends StatelessWidget {
-  const FamilyKitchenApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => AuthProvider()),
-      ],
-      child: MaterialApp(
-        title: 'Family Kitchen - The Royal Hearth',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-          useMaterial3: true,
-          fontFamily: 'Roboto',
-        ),
-        home: const SplashScreen(),
-        routes: appRoutes,
-      ),
-    );
-  }
 }
